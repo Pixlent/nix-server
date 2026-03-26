183fc53937f629cf4c0c3f40e3845b355e5689ed
Alexander <2beheima@gmail.com>
2026-03-26 19:49:59 +0100

    Update base.nix

--- common/base.nix
+++ common/base.nix
@@ -12,13 +12,13 @@
   time.timeZone = "Europe/Oslo";
 
   # Select internationalisation properties.
-  i18n.defaultLocate = "en_US.UTF-8";
+  #i18n.defaultLocate = "en_US.UTF-8";
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "no";
-    variant = "";
-  };
+  #services.xserver.xkb = {
+  #  layout = "no";
+  #  variant = "";
+  #};
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
