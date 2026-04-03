57a8ffe6e99c2ddf0a2c873e49a9e2581df3802d
Alexander <2beheima@gmail.com>
2026-04-03 19:15:29 +0200

    feat: Add bootloader to chandler default.nix

--- hosts/chandler/default.nix
+++ hosts/chandler/default.nix
@@ -4,6 +4,11 @@
     ./hardware-configuration.nix
   ];
 
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
   networking.hostName = "chandler";
 
   users.users.chandler = {
