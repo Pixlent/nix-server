a3c71c251265d635759dcfb313096e4e24a0c3ad
Alexander <2beheima@gmail.com>
2026-04-03 20:27:32 +0200

    Update btrfs-raid.nix

--- hosts/chandler/btrfs-raid.nix
+++ hosts/chandler/btrfs-raid.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }: {
   boot.supportedFilesystems = [ "btrfs" ];
-
-  # Pre-format first: mkfs.btrfs -f -d raid1 -m raid1 /dev/disk/by-id/scsi-0QEMU...1 /dev/disk/by-id/scsi-0QEMU...2
+  environment.systemPackages = [ pkgs.btrbk ];
 
   fileSystems."/tank" = {
     device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1";
@@ -11,13 +10,14 @@
 
   services.btrbk.instances."tank" = {
     onCalendar = "daily";
-    path = "/tank";
-    target = "/tank/.snapshots";
-    snapshotDir = "/tank/.snapshots";
     settings = {
       snapshot_preserve_min = "7d";
       snapshot_preserve = "7d 4w 3m";
       preserve_snapshots = true;
+
+      volume "/tank" {
+        snapshot_dir "/tank/.snapshots";
+      }
     };
   };
 }
